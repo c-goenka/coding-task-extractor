fc6209ee1ce003ce51c1a8a080cc19fe63b1c1d4
chetan <cgoenka30@gmail.com>
2025-07-08 14:22:00 +0530

    data processor clean up

--- src/data_processor.py
+++ src/data_processor.py
@@ -4,11 +4,7 @@ import pandas as pd
 class DataProcessor:
     def __init__(self, config):
         self.config = config
-
-    def process_papers(self, papers_csv_path):
-        papers_df = pd.read_csv(papers_csv_path)
-
-        column_rename_mapping = {
+        self.column_rename_mapping = {
             'Key' : 'paper_id',
             'Title' : 'title',
             'Author' : 'authors',
@@ -19,14 +15,16 @@ class DataProcessor:
             'Abstract Note' : 'abstract',
             'File Attachments' : 'pdf_path'
         }
+        self.selected_columns = ['paper_id', 'title', 'authors', 'venue', 'year', 'isbn', 'url', 'abstract', 'pdf_path']
 
-        papers_df.rename(columns=column_rename_mapping, inplace=True)
+    def process_papers(self, papers_csv_path):
+        papers_df = pd.read_csv(papers_csv_path)
+        papers_df = papers_df.rename(columns=self.column_rename_mapping)
 
         keyword_filter = '|'.join(self.config.FILTER_KEYWORDS)
-        papers_df = papers_df[papers_df['abstract'].str.contains(keyword_filter)]
+        papers_df = papers_df[papers_df['abstract'].str.contains(keyword_filter, case=False, na=False)]
 
-        selected_columns = ['paper_id', 'title', 'authors', 'venue', 'year', 'isbn', 'url', 'abstract', 'pdf_path']
-        papers_dict = papers_df[selected_columns].set_index('paper_id').to_dict(orient='index')
+        papers_dict = papers_df[self.selected_columns].set_index('paper_id').to_dict(orient='index')
 
         output_path = self.config.DATA_DIR / "papers_dict.json"
         with open(output_path, 'w') as f:
